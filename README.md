238d64cf43878be7130996d2e1fac8d2feacec72
Adeel Safdar <adeelsafdar007@gmail.com>
2022-07-06 23:43:31 +0500

    Finalizing Chnges

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Adeel Search Engine
  
+Installation:
+
+1) Open cmd as Administrator
+2) Goto to root directory
+3) Create an empty file called "composer.json"
+4) Run "composer require adeel/hello-composer:dev-main" command
+
+Usage:
+
+After installation include following lines in your respected php file.
+
+require_once __DIR__ . '/vendor/autoload.php';
+use adeel\searchengine\SearchEngine;
+
+$client = new SearchEngine();
+
+//You can set your search engine here
+$client->setEngine("google.com");
+
+//Pass your keywords as an array in the following line
+$results = $client->search(["best hosting","best movie"]);
+
+echo "<pre>";
+
+// Your results will be available in $results variable as an array having the status of the operation and the data retrieved
+var_dump($results); 
+
+echo "</pre>";
--- src/searchengine.php
+++ src/searchengine.php
@@ -1,6 +1,7 @@
-<?php 
-
+<?php
 namespace adeel\searchengine;
+use DOMDocument;
+use DOMXPath;
 
 class SearchEngine
 {
@@ -12,15 +13,119 @@ class SearchEngine
         $this->searchengine = $searchengine;
     }
 
-    public function search($keywords = array('best game'))
+    public function search($keywords = array(
+        'best game'
+    ))
     {
-         require_once "vendor/autoload.php";
-         $httpClient = new \GuzzleHttp\Client();
-        $response = $httpClient->get('https://www.google.com/search?q=best+hosting');
-        $htmlString = (string) $response->getBody();
+        require_once "vendor/autoload.php";
+        $httpClient = new \GuzzleHttp\Client();
 
-        echo "<pre>";
-        print_r($htmlString);
-        echo "</pre>";
+        $combined = array();
+        $combinedarr_count = 0;
+        
+
+        foreach ($keywords as $key => $keyword)
+        {
+
+            $ranking_count = 1;
+            $searchdescriptionsarr = array();
+
+            $response = $httpClient->get($this->searchengine . '/search?q=' . $keyword . '', ['http_errors' => false]);
+            $statuscode = $response->getStatusCode();
+
+            if (200 === $statuscode)
+            {
+
+            }
+            elseif (304 === $statuscode)
+            {
+                return array(
+                    'status' => 'error',
+                    'message' => "304 Not Modified"
+                );
+                die();
+            }
+            elseif (404 === $statuscode)
+            {
+                return array(
+                    'status' => 'error',
+                    'message' => "Page not Found"
+                );
+                die();
+            }
+            else
+            {
+                return array(
+                    'status' => 'error',
+                    'message' => "Something went wrong"
+                );
+                die();
+            }
+
+            for ($pagestart = 0;$pagestart <= (10 * 5);$pagestart += 10)
+            {
+                $response = $httpClient->get($this->searchengine . '/search?q=' . $keyword . '&start=' . $pagestart, ['http_errors' => false]);
+
+                $htmlString = (string)$response->getBody();
+
+                libxml_use_internal_errors(true);
+                $doc = new DOMDocument();
+                $doc->loadHTML($htmlString);
+                $xpath = new DOMXPath($doc);
+
+                $adstitles = $xpath->evaluate('//*[@id="main"]//div//div//div//div//a//div[@role="heading"]//span');
+                $adsdescriptions = $xpath->evaluate('//*[@id="main"]//div//div//div//div[@class="w1C3Le"]//div//div');
+                $adslinks = $xpath->evaluate('//*[@id="main"]//div//div//div//div//a//div//span[@role="text"]');
+
+                $searchtitles = $xpath->evaluate('//*[@id="main"]//div//div//div[@class="egMi0 kCrYT"]//a//h3[@class="zBAuLc l97dzf"]//div');
+                $searchdescriptions = $xpath->evaluate('//*[@id="main"]/div/div[@class="Gx5Zad fP1Qef xpd EtOod pkphOe"]/div[@class="kCrYT"]/div/div[@class="BNeawe s3v9rd AP7Wnd"]/div');
+                $searchlinks = $xpath->evaluate('//*[@id="main"]//div//div//div[@class="egMi0 kCrYT"]//a//@href');
+
+                foreach ($searchdescriptions as $key => $single)
+                {
+                    if (!in_array($single->textContent, array(
+                        ' · '
+                    )))
+                    {
+                        $searchdescriptionsarr[] = $single->textContent;
+                    }
+                }
+
+                foreach ($adstitles as $key => $single)
+                {
+                    $combined[$combinedarr_count]['keyword'] = $keyword;
+                    $combined[$combinedarr_count]['title'] = $single->textContent;
+                    $combined[$combinedarr_count]['description'] = $adsdescriptions[$key]->textContent;
+                    $combined[$combinedarr_count]['link'] = $adslinks[$key]->textContent;
+                    $combined[$combinedarr_count]['promoted'] = 1;
+                    $combined[$combinedarr_count]['ranking'] = $ranking_count;
+
+                    $combinedarr_count++;
+                    $ranking_count++;
+                }
+
+                foreach ($searchtitles as $key => $single)
+                {
+                    $combined[$combinedarr_count]['keyword'] = $keyword;
+                    $combined[$combinedarr_count]['title'] = $single->textContent;
+                    $combined[$combinedarr_count]['description'] = $searchdescriptionsarr[$key];
+                    $combined[$combinedarr_count]['link'] = str_replace('/url?q=', '', $searchlinks[$key]->textContent);
+                    $combined[$combinedarr_count]['promoted'] = 0;
+                    $combined[$combinedarr_count]['ranking'] = $ranking_count;
+
+                    $combinedarr_count++;
+                    $ranking_count++;
+                }
+
+            }
+
+        }
+
+        return array(
+            'status' => 'success',
+            'data' => $combined
+        );
+
     }
 }
+
